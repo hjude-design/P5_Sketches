dd21323397ca2dd40be2d468da700b14d8abb229
hjude-design <harrison@hjude.com>
2022-10-16 08:02:56 -0400

    Basic formatting and updated script tag

--- sk_004/script.js
+++ sk_004/script.js
@@ -1,54 +1,26 @@
-const rings = [];
-let ringCount;
-let baseGap = 50;
-
-let mPosX;
-let mDist;
+let words = [
+    'hey',
+    'you',
+    'lets',
+    'create',
+    'someting',
+    'remarkable'
+];
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
 
-    if(window.innerWidth>window.innerHeight){
-        ringCount = window.innerWidth/baseGap;
-        ringCount *= 1.5;
-        console.log(ringCount);
-    }else{
-        console.log('confused');
-    }
+    background(200);
 
-    for(i=0; i<ringCount; i++){
-        rings.push(new RING(i));
-    }
+    console.log(words);
+    
 }
 function draw(){
-    clear();
-
-    mPosX = (mouseX / window.innerWidth) - .5;
-
-    translate(width/2, height/2);
-
-    mDist = dist(width/2,height/2,mouseX,mouseY);
-
-    rings.forEach((ring)=>{
-        ring.create();
-    })
-    
 }
 
-class RING{
-    constructor(index){
-        this.indx = index;
-
-        this.rad = baseGap * (this.indx+1);
-
+class phraseChunk{
+    constructor(words){
+        this.words = words;
     }
-    
-    create(){
-        noFill();
-        strokeWeight(mDist/100);
-        rotate(.5*mPosX);
-        ellipse(0, 0, this.rad, this.rad*1.5);
-    }
-
 
 }
