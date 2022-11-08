35d474050047d5684cbd09d815020033cc45d8a8
hjude-design <harrison@hjude.com>
2022-11-08 12:17:55 -0500

    Made responsive layout for horizontal screens

--- sk_004/script.js
+++ sk_004/script.js
@@ -7,30 +7,30 @@ const words = [
     ['something'],
     [`${adjectives[Math.round(Math.random()*(adjectives.length-1))]}!`]
 ];
+
 let Pad;
 let spacer;
 let barHeight = 80;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-
-    Pad = window.innerWidth/20;
+    Warble = createGraphics(100, 100);
+    Pad = width/20;
 
     textFont('IBM Plex Serif');
     textStyle(BOLDITALIC);
-
     barHeight = 80;
-
-
     noStroke();
-
     blendMode(SCREEN);
 
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    Warble = createGraphics(windowWidth, windowHeight);
+    Pad = width/20;
   }
+
 
 function draw(){
     clear();
@@ -45,56 +45,93 @@ function draw(){
 function drawIntro(spacer, barHeight){
 
     lineWidth = window.innerWidth - Pad*2;
-
     spacer = -1 * textWidth('.');
-
     textSize(windowHeight/10);
-
-    words.forEach(line=>{
-        const lineIndex = words.indexOf(line);
-
-        const lineHeight = Pad + lineIndex*(textAscent()+20);
-
-        let wordWidth = 0;
-
-        let leftInset = Pad;
-
-        let gap = {
-            start:Pad,
-            end:lineWidth + Pad
-        }
-        line.forEach(word=>{
-            
-            if(line.length > 1 && line.indexOf(word) < 1){
-                textAlign(LEFT);
-                gap.start += textWidth(word) - spacer;
-            }else if(line.length > 1){
-                textAlign(RIGHT);
-                gap.end -= textWidth(word) - spacer;
-            }else if(lineIndex % 2 === 0){
-                textAlign(LEFT);
-                leftInset += textWidth(word) + spacer;
-                gap.start += textWidth(word) - spacer;
-            }else if(lineIndex % 2 !== 0){
-                textAlign(RIGHT);
-                gap.end -= textWidth(word) - spacer;
+    
+    const phrase = [];
+    
+    if (height < width) {
+        words.forEach(line=>{
+        
+            const lineIndex = words.indexOf(line);
+            const lineHeight = Pad + lineIndex*(textAscent()+20);
+    
+            let wordWidth = 0;
+            let leftInset = Pad;
+            let gap = {
+                start:Pad,
+                end:lineWidth + Pad
             }
+    
+            line.forEach(word=>{
+                
+                if(line.length > 1 && line.indexOf(word) < 1){
+                    textAlign(LEFT);
+                    gap.start += textWidth(word) - spacer;
+                }else if(line.length > 1){
+                    textAlign(RIGHT);
+                    gap.end -= textWidth(word) - spacer;
+                }else if(lineIndex % 2 === 0){
+                    textAlign(LEFT);
+                    leftInset += textWidth(word) + spacer;
+                    gap.start += textWidth(word) - spacer;
+                }else if(lineIndex % 2 !== 0){
+                    textAlign(RIGHT);
+                    gap.end -= textWidth(word) - spacer;
+                }
+    
+                fill(30,30,30);
+                text(word,Pad,lineHeight,lineWidth);
+    
+                wordWidth += textWidth(word);
+            });
+    
+            barXHeight = textAscent()*(barHeight/100);
+            const barYpos = lineHeight + textAscent()/2 - barXHeight/2;
+    
+            const gapAvail = gap.end - gap.start;
+            const barXWidth = Math.abs(gapAvail);
+    
+            fill('#ff0000');
+            rect(gap.start,barYpos,barXWidth,barXHeight);
+    
+        })
+    }
+    
 
-            fill(30,30,30);
-            text(word,Pad,lineHeight,lineWidth);
+    if (width < height) {
 
-            wordWidth += textWidth(word);
-        });
+        words.forEach(line=>{
+            line.forEach(word=>{
+            phrase.push(word);
+            });
 
-        barXHeight = textAscent()*(barHeight/100);
-        const barYpos = lineHeight + textAscent()/2 - barXHeight/2;
+        })
+        
+    
+        phrase.forEach(word=>{
 
-        const gapAvail = gap.end - gap.start;
-        console.log(gap, gapAvail);
-        const barXWidth = Math.abs(gapAvail);
+            textSize(windowWidth/8);
+            const lineHeight = textAscent() + Pad + phrase.indexOf(word)*(textAscent()
+            *1.25);
+    
+            fill(0);
+            textAlign(LEFT);
+            text(word, Pad, lineHeight)
+            
+            fill(255,0,0);
+            const barYHeight = textAscent()*(barHeight/100);
+            const barYPos = lineHeight - textAscent() + barYHeight/3;
+    
+            rect(Pad + textWidth(word) - spacer*2, barYPos, width, barYHeight);
 
-        fill('#ff0000');
-        rect(gap.start,barYpos,barXWidth,barXHeight);
 
-    })
+        })
+    
+
+    }
+
+
+    
+
 }
